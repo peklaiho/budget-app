f84e33595829dd2fecb5c48e72d5b16964fceaaa
Pekka Laiho <pekka.i.laiho@gmail.com>
2020-03-28 12:50:50 +0700

    balances on home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,10 +3,36 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Cake\Datasource\ConnectionManager;
+
 class HomeController extends AppController
 {
     public function index()
     {
+        $conn = ConnectionManager::get('default');
 
+        $sql = "SELECT ac.id, ac.name, ac.start_money, " .
+            "(SELECT SUM(amount) FROM expenses AS e WHERE e.account_id = ac.id) as expenses, " .
+            "(SELECT SUM(amount) FROM incomes AS i WHERE i.account_id = ac.id) as income, " .
+            "(SELECT SUM(amount) FROM transfers AS tf WHERE tf.from_account_id = ac.id) as transfer_from, " .
+            "(SELECT SUM(amount) FROM transfers AS tf WHERE tf.to_account_id = ac.id) as transfer_to " .
+            "FROM accounts AS ac ORDER BY ac.name";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        $rows = $stmt->fetchAll('assoc');
+
+        $accounts = [];
+
+        foreach ($rows as $row) {
+            $accounts[] = [
+                'id' => $row['id'],
+                'name' => $row['name'],
+                'balance' => $row['start_money'] + $row['income'] + $row['transfer_to'] - $row['expenses'] - $row['transfer_from']
+            ];
+        }
+
+        $this->set('accounts', $accounts);
     }
 }
--- templates/Home/index.php
+++ templates/Home/index.php
@@ -1,1 +1,22 @@
-home page
+<h2>Balances</h2>
+
+<div class="row mt-4">
+    <div class="col-6">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Account</th>
+                    <th class="text-right">Balance</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($accounts as $account): ?>
+                    <tr>
+                        <td><?= $account['name'] ?></td>
+                        <td class="text-right"><?= number_format($account['balance'], 2, '.', ' ') ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</div>
