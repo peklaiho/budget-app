e27cce3652fce093e1169c2133bc83c820173ce0
Pekka Laiho <pekka.i.laiho@gmail.com>
2020-12-30 10:26:57 +0700

    update composer packages

--- config/routes.php
+++ config/routes.php
@@ -10,7 +10,7 @@ $routes->setRouteClass(DashedRoute::class);
 $routes->scope('/', function (RouteBuilder $builder) {
     // Register scoped middleware for in scopes.
     $builder->registerMiddleware('csrf', new CsrfProtectionMiddleware([
-        'httpOnly' => true,
+        'httponly' => true,
     ]));
     $builder->applyMiddleware('csrf');
 
